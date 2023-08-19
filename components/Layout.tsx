76fe4e0ca135deef2c568699127901a9302ee658
vinicios <vinigadd@gmail.com>
2023-08-19 11:13:04 -0300

    refactor: project setup

---
+++ components/Layout.tsx
@@ -0,0 +1,22 @@
+import Sidebar from "./layout/Sidebar";
+
+type LayoutProp = {
+  children: React.ReactNode;
+};
+
+function Layout({ children }: LayoutProp) {
+  return (
+    <div className="h-screen bg-black">
+      <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+        <div className="grid grid-cols-4 h-full">
+          <Sidebar />
+          <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
