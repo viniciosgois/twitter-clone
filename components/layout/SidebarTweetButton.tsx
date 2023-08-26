31f531e36cf372b54e68b90e0940124017d9a1f7
vinicios <vinigadd@gmail.com>
2023-08-26 16:05:29 -0300

    refactor: ensure the correct modal is called

--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -1,10 +1,17 @@
-import { useRouter } from 'next/router';
+import { useCallback } from 'react';
 import { FaFeather } from 'react-icons/fa';
 
+import useLoginModal from '@/hooks/useLoginModal';
+
 function SidebarTweetButton() {
-  const router = useRouter();
+  const loginModal = useLoginModal();
+
+  const onClick = useCallback(() => {
+    loginModal.onOpen();
+  }, [loginModal]);
+
   return (
-    <div onClick={() => router.push('/')}>
+    <div onClick={onClick}>
       <div className='mt-6 flex h-14 w-14 cursor-pointer items-center justify-center rounded-full bg-sky-500 p-4 transition hover:bg-opacity-80 lg:hidden'>
         <FaFeather size={24} color='white' />
       </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
 import type { AppProps } from 'next/app';
 
+import LoginModal from '@/components/modals/LoginModal';
 import Layout from '@/components/Layout';
 
 import '@/styles/globals.css';
+import RegisterModal from '@/components/modals/RegisterModal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
+    <>
+      <RegisterModal />
+      <LoginModal />
+      <Layout>
+        <Component {...pageProps} />;
+      </Layout>
+    </>
   );
 }
